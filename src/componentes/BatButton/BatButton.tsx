adf39af298c3bab6b8e1ca4c26a1b11804eda76c
urbanodev81 <ricardo@focusmedia.com.br>
2024-07-29 11:52:17 -0300

    projeto gerador de senha finalizado

--- src/componentes/BatButton/BatButton.tsx
+++ src/componentes/BatButton/BatButton.tsx
@@ -1,25 +1,34 @@
 import React, { useState } from 'react';
-import {Button, Pressable, Text, View } from 'react-native';
+import {Pressable, Text, View } from 'react-native';
+
+import * as Clipboard from 'expo-clipboard';
 
 import { styles } from './BatButtonStyle';
 import { BatTextInput } from '../BatTextInput/BatTextInput';
+import generatePass from '../../services/password.Service';
 
 export function BatButton() {
   const [pass, setPass] = useState('');
 
-  function hadleGenerateButton () {
-    setPass('ola povo');
+  function handleGenerateButton () {
+    let generateToken = generatePass();
+    setPass(generateToken);
+  }
+
+  function handleCopuButtoon(){
+    Clipboard.setStringAsync(pass);
+
   }
   return (
     <View style={styles.container}>
         <BatTextInput pass={pass}  />
 
         <Pressable style={styles.buttonGerar}
-           onPress={hadleGenerateButton}>
+           onPress={handleGenerateButton}>
           <Text  style={styles.textGerar}>Gerar</Text>
         </Pressable>
         <Pressable style={styles.buttonCopy}
-           onPress={() => {console.log('presssionado copy')}}>
+           onPress={handleCopuButtoon}>
           <Text  style={styles.textCopy}>COPY</Text>
         </Pressable>
     </View>
